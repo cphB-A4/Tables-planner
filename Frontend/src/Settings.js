2b401b37eedb3ab841c9c827b1c6437dabf0bc24
MustafaTokmak1 <cph-mt357@cphbusiness.dk>
2022-03-27 11:42:57 +0200

    first try cd/ci pipeline

--- Frontend/src/Settings.js
+++
@@ -1,3 +0,0 @@
-const URL = "http://localhost:8080/tables-planner";
-//http://localhost:8080/tables-planner/api/info/populate
-export { URL };
--- Frontend/src/components/NoMatch.js
+++
@@ -1,5 +0,0 @@
-function NoMatch() {
-  return <div>No Match - code 404</div>;
-}
-
-export default NoMatch;
